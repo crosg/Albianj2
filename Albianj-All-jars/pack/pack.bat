44b0c805e3577ff77a49b0123c4cc63e93d586c8
大嘴 <xvhfeng@gmail.com>
2016-05-05 10:33:20 +0800

    增加文件头版权信息，restful增加null字段的支持，albianj的isalbiannew字段不被序列化

--- Albianj-All-jars/pack/pack.bat
+++ Albianj-All-jars/pack/pack.bat
@@ -1,5 +1,5 @@
-java -Djava.ext.dirs=E:\sync\tencent\java\dev\albianJars2.0\jars -jar E:\sync\tencent\java\dev\albianJars2.0\pack\Albianj.Packing.jar E:\sync\tencent\java\dev\albianJars2.0\impl
+java -Djava.ext.dirs=E:\sync\tencent\github\albianj\Albianj-All-jars\jars -jar E:\sync\tencent\github\albianj\Albianj-All-jars\pack\Albianj.Packing.jar E:\sync\tencent\github\albianj\Albianj-All-jars\impl
 echo "packing is over ,then copy..."
-copy /y E:\sync\tencent\java\dev\albianJars2.0\impl\Albianj.spx E:\sync\tencent\java\dev\albianJars2.0\jars
+copy /y E:\sync\tencent\github\albianj\Albianj-All-jars\impl\Albianj.spx E:\sync\tencent\github\albianj\Albianj-All-jars\jars
 echo "copy end"
 PAUSE
