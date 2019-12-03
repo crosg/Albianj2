7789712309855baeedae897b0b9d63cc101094b2
spkxu <xvhfeng@gmail.com>
2019-12-03 14:23:06 +0800

    fixbug：when call copyobject，maybe function called will be stack overflow

--- Albianj.Persistence/src/main/java/org/albianj/persistence/service/AlbianObjectCreator.java
+++ Albianj.Persistence/src/main/java/org/albianj/persistence/service/AlbianObjectCreator.java
@@ -89,7 +89,7 @@ public class AlbianObjectCreator {
     }
 
     public static void copyObject(IAlbianObject dest,IAlbianObject src){
-        copyObject(dest,src);
+        copyObject(dest,src,null);
     }
 
     /**
